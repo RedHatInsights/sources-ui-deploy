3109178c11a81cd4a9a423727bfdcc0868fec487
Travis CI User <travis@example.org>
2021-02-22 07:49:54 +0000

    Merge pull request #590 from rvsia/moveWizardBackToUI
    Move wizard code back to the UI

--- js/2.a04cfcdd8b79e8ff832e.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[2],{93002:(e,t,r)=>{"use strict";r.r(t);var n,u=r(80150),s=r.n(u),c=r(70695),a=r.n(c),o=r(87016),l=r(27542),i=r(88144);n||(n=(0,i.X5)());const d=function(){return s().createElement(o.Provider,{store:n},s().createElement(l.Z,null))};var f=document.getElementById("root");a().render(s().createElement(d,null),f,(function(){return f.setAttribute("data-ouia-safe",!0)}))}}]);
-//# sourceMappingURL=2.a04cfcdd8b79e8ff832e.js.map
--- js/374.05e97b0d3deb7b3bef2f.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[374],{41374:(e,r,u)=>{"use strict";u.r(r),u.d(r,{default:()=>o});var s,t=u(80150),n=u.n(t),c=u(87016),l=u(27542),a=u(88144);s||(s=(0,a.X5)());const o=function(){return n().createElement(c.Provider,{store:s},n().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=374.05e97b0d3deb7b3bef2f.js.map

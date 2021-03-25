21b0db6088f7f9149d042c138da98f27e929682c
Travis CI User <travis@example.org>
2021-03-25 12:53:16 +0000

    Merge pull request #633 from rvsia/updateToReact17
    Update to React 17

--- js/1374.60be68b24bfa762f5954.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[1374],{41374:(e,r,u)=>{"use strict";u.r(r),u.d(r,{default:()=>o});var s,t=u(80150),n=u.n(t),c=u(557),l=u(27542),a=u(88144);s||(s=(0,a.X5)());const o=function(){return n().createElement(c.Provider,{store:s},n().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=1374.60be68b24bfa762f5954.js.map

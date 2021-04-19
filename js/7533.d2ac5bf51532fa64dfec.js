730a897394961b475a182b70041dc874b015c58b
Travis CI User <travis@example.org>
2021-04-19 15:03:36 +0000

    Merge branch 'stable' into prod-beta

--- js/7533.d2ac5bf51532fa64dfec.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[7533],{57533:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(39324);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
-//# sourceMappingURL=7533.d2ac5bf51532fa64dfec.js.map

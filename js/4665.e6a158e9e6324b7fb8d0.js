6f0059cd2becb25d0bdc3e03de9a3ac2da754d6a
Travis CI User <travis@example.org>
2021-04-22 10:50:44 +0000

    Merge pull request #658 from rvsia/updateMobileActions
    Use dropdown item on mobile screen instead of button

--- js/4665.e6a158e9e6324b7fb8d0.js
+++ js/4665.e6a158e9e6324b7fb8d0.js
@@ -1,1 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[4665],{14665:(e,u,t)=>{"use strict";function s(e,u){return(s=Object.setPrototypeOf||function(e,u){return e.__proto__=u,e})(e,u)}t.d(u,{Z:()=>s})}}]);
+//# sourceMappingURL=../sourcemaps/4665.985ae77dd82dba0ce17c.js.map

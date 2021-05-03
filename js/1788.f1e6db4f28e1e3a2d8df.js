1ba0b224bdf6734fea7dbcff73562326ee80235d
Travis CI User <travis@example.org>
2021-05-03 12:57:06 +0000

    Merge pull request #666 from rvsia/addIconForProgressStatus
    Add icon for progress status

--- js/1788.f1e6db4f28e1e3a2d8df.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[1788],{41788:(e,t,o)=>{"use strict";o.d(t,{Z:()=>r});var c=o(14665);function r(e,t){e.prototype=Object.create(t.prototype),e.prototype.constructor=e,(0,c.Z)(e,t)}}}]);
-//# sourceMappingURL=../sourcemaps/1788.db43b7e190960adf5b10.js.map

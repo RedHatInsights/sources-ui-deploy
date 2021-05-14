b8ccde63ceffeaa56f4c32c6ccae9444d027a999
Travis CI User <travis@example.org>
2021-05-14 06:49:41 +0000

    Merge pull request #668 from rvsia/removeAppRemoveModal
    Remove RemoveAppModal

--- js/4665.54c84fab4e3984b0ce5a.js
+++ js/4665.54c84fab4e3984b0ce5a.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[4665],{14665:(e,u,t)=>{"use strict";function s(e,u){return(s=Object.setPrototypeOf||function(e,u){return e.__proto__=u,e})(e,u)}t.d(u,{Z:()=>s})}}]);
-//# sourceMappingURL=../sourcemaps/4665.200bf634aed790a36b64.js.map
+//# sourceMappingURL=../sourcemaps/4665.5ecc84dc4fe220e23ecd.js.map

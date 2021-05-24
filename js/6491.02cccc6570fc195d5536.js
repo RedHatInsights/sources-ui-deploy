e2587eb14c7c2118f9de8b347cd29eac01fbd9da
Travis CI User <travis@example.org>
2021-05-24 15:38:05 +0000

    Merge pull request #681 from rvsia/fixAppKebabPading
    Fix app kebab padding

--- js/6491.02cccc6570fc195d5536.js
+++ js/6491.02cccc6570fc195d5536.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[6491],{36491:(e,t,r)=>{"use strict";r.r(t);var n,u=r(92950),s=r.n(u),c=r(12181),a=r.n(c),o=r(45237),l=r(32610),i=r(17711);n||(n=(0,i.X5)());const d=function(){return s().createElement(o.Provider,{store:n},s().createElement(l.Z,null))};var f=document.getElementById("root");a().render(s().createElement(d,null),f,(function(){return f.setAttribute("data-ouia-safe",!0)}))}}]);
-//# sourceMappingURL=../sourcemaps/6491.f162b0f530a0246cb3c2.js.map
+//# sourceMappingURL=../sourcemaps/6491.d4d2a3e0c9ec3942e932.js.map

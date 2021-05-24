5cd805b2f35cee034415ef9886ccf00dabc741dd
Travis CI User <travis@example.org>
2021-05-24 10:59:05 +0000

    Merge pull request #679 from rvsia/addAppRemoveBack
    Add application kebab

---
+++ js/5185.c22f0a2a169e10951ad7.js
@@ -0,0 +1,2 @@
+(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[5185],{15185:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(98495);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
+//# sourceMappingURL=../sourcemaps/5185.68755ca5a77bed29cefa.js.map
--- js/8412.cd8ddba409c6d656a312.js
+++ js/8412.cd8ddba409c6d656a312.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[8412],{28412:(e,r,u)=>{"use strict";u.r(r),u.d(r,{default:()=>o});var s,t=u(92950),n=u.n(t),c=u(45237),l=u(32610),a=u(17711);s||(s=(0,a.X5)());const o=function(){return n().createElement(c.Provider,{store:s},n().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/8412.2350eb7f30f862f8e531.js.map
+//# sourceMappingURL=../sourcemaps/8412.68755ca5a77bed29cefa.js.map

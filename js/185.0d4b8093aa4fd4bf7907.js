afe12da6c9c8484f146b707931ccc640e4fefef5
Travis CI User <travis@example.org>
2021-06-09 16:44:05 +0000

    Merge pull request #702 from rvsia/fixNavigationEvenet
    Fix navigation event listener

--- js/185.0d4b8093aa4fd4bf7907.js
+++ js/185.0d4b8093aa4fd4bf7907.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[185],{15185:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(98495);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
-//# sourceMappingURL=../sourcemaps/185.0a48b16bbd7735221659.js.map
+//# sourceMappingURL=../sourcemaps/185.46fd4df18b3ecca39007.js.map

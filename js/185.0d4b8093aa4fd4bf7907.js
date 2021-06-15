69aa41abcb368e17e91588da371014b7666f1e11
Travis CI User <travis@example.org>
2021-06-15 12:56:00 +0000

    Merge pull request #708 from adberglund/cost-update-aws-policy-json
    Switch headbucket to the correct listbucket

--- js/185.0d4b8093aa4fd4bf7907.js
+++ js/185.0d4b8093aa4fd4bf7907.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[185],{15185:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(98495);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
-//# sourceMappingURL=../sourcemaps/185.ea7ef26119c9e6da80b5.js.map
+//# sourceMappingURL=../sourcemaps/185.3cc143a24a75bc08cdba.js.map
--- js/412.9bfcc0e54d3712666a9a.js
+++ js/412.9bfcc0e54d3712666a9a.js
@@ -1,2 +1,2 @@
 (self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[412],{28412:(e,r,u)=>{"use strict";u.r(r),u.d(r,{default:()=>o});var s,t=u(92950),n=u.n(t),c=u(45237),l=u(68309),a=u(17711);s||(s=(0,a.X5)());const o=function(){return n().createElement(c.Provider,{store:s},n().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/412.ea7ef26119c9e6da80b5.js.map
+//# sourceMappingURL=../sourcemaps/412.3cc143a24a75bc08cdba.js.map

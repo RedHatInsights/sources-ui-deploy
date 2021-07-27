f80dc6e4ba08286200a5030883fcf6eb3b6d12f1
Travis CI User <travis@example.org>
2021-07-27 13:49:43 +0000

    Merge pull request #729 from rvsia/updateDeps1232
    Update dependencies and readme title

---
+++ js/185.d9b51c6d8d8330434797.js
@@ -0,0 +1,2 @@
+(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[185],{15185:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(98495);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
+//# sourceMappingURL=../sourcemaps/185.367078bff5fb55837632.js.map

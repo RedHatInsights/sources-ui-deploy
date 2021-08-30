74e6487d9c31f8c2f5e026e6786fad0bb0cfa1f7
Travis CI User <travis@example.org>
2021-08-30 10:01:32 +0000

    Merge pull request #741 from rvsia/updateMobileTable
    UseMobileLayout in PrimaryToolbar

--- js/185.d9b51c6d8d8330434797.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[185],{15185:(e,s,a)=>{"use strict";a.d(s,{Z:()=>o});var r=a(98495);const o=function(e,s,a,o,u){var n=s.formatMessage({id:"sources.removeAppWarning",defaultMessage:"{ name } was removed from this source."},{name:e.display_name}),i=s.formatMessage({id:"sources.removeAppError",defaultMessage:"Removing of { name } application from this source was unsuccessful."},{name:e.display_name});return a&&a(),o((0,r.Yn)(e.id,u.id,n,i))}}}]);
-//# sourceMappingURL=../sourcemaps/185.3295d71decd91a7e0236.js.map

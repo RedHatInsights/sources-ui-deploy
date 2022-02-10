9d647b1ecca528f8952ac15b30fccb48996e6189
Travis CI User <travis@example.org>
2022-02-10 15:55:01 +0000

    Merge pull request #804 from rvsia/addIbmType
    [RHCLOUD-17554] Add IBM type to wizard

---
+++ js/4716.1644508426938.67a92b55991a3f5a26c5.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[4716],{36491:(e,t,n)=>{n.r(t);var r,u=n(92950),s=n.n(u),c=n(12181),a=n.n(c),l=n(42396),o=n(68309),i=n(17711);r||(r=(0,i.X5)());const f=function(){return s().createElement(l.zt,{store:r},s().createElement(o.Z,null))};var m=document.getElementById("root");a().render(s().createElement(f,null),m,(function(){return m.setAttribute("data-ouia-safe",!0)}))}}]);
+//# sourceMappingURL=../sourcemaps/4716.7cdada3c17b1c745e292c1c3df4a2c2c.js.map

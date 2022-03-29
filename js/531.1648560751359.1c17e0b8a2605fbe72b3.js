6f4faa6791c4eda40bddba12c240bb1db220e32a
Travis CI User <travis@example.org>
2022-03-29 13:33:56 +0000

    Merge pull request #856 from rvsia/removeDescriptionFromApps
    Update applications card

---
+++ js/531.1648560751359.1c17e0b8a2605fbe72b3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[531],{36491:(e,t,n)=>{n.r(t);var r,u=n(96985),s=n.n(u),c=n(20563),a=n.n(c),l=n(42396),o=n(68309),i=n(17711);r||(r=(0,i.X5)());const f=function(){return s().createElement(l.zt,{store:r},s().createElement(o.Z,null))};var m=document.getElementById("root");a().render(s().createElement(f,null),m,(function(){return m.setAttribute("data-ouia-safe",!0)}))}}]);
+//# sourceMappingURL=../sourcemaps/531.0476129d7722dc704fb2eebef75b3f44.js.map

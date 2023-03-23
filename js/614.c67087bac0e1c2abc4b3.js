aec2a51b16efd6e30131a2a3f6a7da3f3757586b
Travis CI User <travis@example.org>
2023-03-23 09:06:56 +0000

    Merge pull request #1042 from chambridge/rhcloud-22885-azure-scopes-updates
    RHCLOUD-22885: Update text for Azure EA and MCA scopes

---
+++ js/614.c67087bac0e1c2abc4b3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[614],{28412:(e,t,u)=>{u.r(t),u.d(t,{default:()=>o});var s,n=u(92950),r=u.n(n),c=u(86706),l=u(68309),a=u(17711);s||(s=(0,a.X5)());const o=function(){return r().createElement(c.zt,{store:s},r().createElement(l.Z,null))}}}]);
+//# sourceMappingURL=../sourcemaps/614.d1ef2ae39b266cf7009e1f725b3ff829.js.map

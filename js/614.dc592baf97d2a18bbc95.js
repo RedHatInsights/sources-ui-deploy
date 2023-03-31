a626bc6f8eeefe533ac30cb8e21916e2444b57e2
Travis CI User <travis@example.org>
2023-03-31 10:09:14 +0000

    Merge pull request #1059 from karelhala/rename-tab-apps
    Rename sources detail tab and apps to HCS when HCS enabled

--- js/614.dc592baf97d2a18bbc95.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[614],{28412:(e,t,u)=>{u.r(t),u.d(t,{default:()=>o});var s,n=u(92950),r=u.n(n),c=u(86706),l=u(68309),a=u(17711);s||(s=(0,a.X5)());const o=function(){return r().createElement(c.zt,{store:s},r().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/614.256fce263abfb7af050a146d61d59551.js.map

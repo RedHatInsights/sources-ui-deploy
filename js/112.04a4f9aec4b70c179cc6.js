8371563bc437a71f9b0620c2752e9fc362d6cf7c
Travis CI User <travis@example.org>
2023-05-30 08:22:25 +0000

    Merge pull request #1095 from Hyperkid123/routing-experiment
    Use global routing configuration.

--- js/112.04a4f9aec4b70c179cc6.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksources_ui=self.webpackChunksources_ui||[]).push([[112],{28412:(e,t,u)=>{u.r(t),u.d(t,{default:()=>o});var s,n=u(92950),r=u.n(n),c=u(86706),l=u(27126),a=u(17711);s||(s=(0,a.X5)());const o=function(){return r().createElement(c.zt,{store:s},r().createElement(l.Z,null))}}}]);
-//# sourceMappingURL=../sourcemaps/112.43fb3c8f163301f5b9b1f99e1d38214a.js.map

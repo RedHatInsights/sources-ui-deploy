165da55ba7838f6d0d7d6990512b771e80acb393
Travis CI User <travis@example.org>
2024-08-14 08:10:18 +0000

    Merge pull request #1230 from karelhala/fix-incorrect-active-content
    Fix incorrect active content tab

--- js/exposed-./RootApp.069d7cfa76748b8b5204.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksources=self.webpackChunksources||[]).push([[8178],{60167:(e,s,t)=>{t.r(s),t.d(s,{default:()=>k});var c=t(28416),l=t.n(c),r=t(86706),u=t(43461),n=t(55874);let a;a||(a=(0,n.X5)());const k=()=>l().createElement(r.zt,{store:a},l().createElement(u.Z,null))}}]);
-//# sourceMappingURL=../../sourcemaps/exposed-./RootApp.62547c6f0b5e0557ed7e42d0fe6ca287.js.map
